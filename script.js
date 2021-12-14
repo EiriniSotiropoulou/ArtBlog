9350e1ac4db82409a8a08ed6350b2f38332f2b19
EiriniSotiropoulou <eirhnhsotiropoulou@gmail.com>
2021-12-14 22:23:07 +0200

    carousel details

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let items = document.querySelectorAll(".carousel .carousel-item");
 
 items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
     }
-})
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+
+// hide show article content on hover
+let descriptions = document.querySelectorAll(".card-img-overlay");
+
+descriptions.forEach((des) => {
+  des.addEventListener(
+    "mouseover",
+    function () {
+      des.classList.add("active");
+    },
+    false
+  );
+  des.addEventListener(
+    "mouseout",
+    function () {
+      des.classList.remove("active");
+    },
+    false
+  );
+});
