9373a423babc796d4206a8545690fe140c0d1f3f
EiriniSotiropoulou <eirhnhsotiropoulou@gmail.com>
2022-01-06 17:56:18 +0200

    fixed carousel interactivity and layout

--- script.js
+++ script.js
@@ -11,20 +11,43 @@ $(document).ready(function () {
 
       itemToClone
         .children(":first-child")
-        .clone()
+        .clone(true, true)
         .addClass("cloneditem-" + i)
         .appendTo($(this));
     }
   });
-});
 
-// edit padding of section
+  // edit padding of section
 
-let sections = document.querySelectorAll("section");
-let navheight = document.querySelectorAll("nav")[0].clientHeight;
-navheight -= 1;
-sections.forEach((sec) => {
-  if (sec.getAttribute("id") != "blog") {
-    sec.style.paddingTop = navheight + "px";
-  }
+  let sections = document.querySelectorAll("section");
+  let navheight = document.querySelectorAll("nav")[0].clientHeight;
+  navheight -= 1;
+  sections.forEach((sec) => {
+    if (sec.getAttribute("id") != "blog") {
+      sec.style.paddingTop = navheight + "px";
+    }
+  });
+
+  //interactivity fo carousel items
+
+  let descriptions = document.querySelectorAll(".card-img-overlay");
+  console.log(descriptions.length);
+  descriptions.forEach((des) => {
+    des.addEventListener(
+      "mouseover",
+      function () {
+        des.classList.add("active");
+        des.previousElementSibling.classList.add("active");
+      },
+      false
+    );
+    des.addEventListener(
+      "mouseout",
+      function () {
+        des.classList.remove("active");
+        des.previousElementSibling.classList.remove("active");
+      },
+      false
+    );
+  });
 });
